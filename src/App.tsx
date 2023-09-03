2d0fe447b6d5c4ba945579b8036026a03fa58561
Varinder Singh <varindersingh6885@email.com>
2023-09-03 21:33:44 +0530

    add color to navbar on scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar.tsx";
 
 const App = () => {
   const [selectedPage, setSelectedPage] = useState("home");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true);
+      else setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div className="app bg-deep-blue">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,6 +8,7 @@ import CloseIcon from "../assets/close-icon.svg";
 type NavbarProps = {
   selectedPage: string;
   setSelectedPage: (val: string) => void;
+  isTopOfPage: boolean;
 };
 
 const links = [
@@ -28,12 +29,17 @@ const links = [
   },
 ];
 
-const Navbar = ({ selectedPage, setSelectedPage }: NavbarProps) => {
+const Navbar = ({
+  selectedPage,
+  setSelectedPage,
+  isTopOfPage,
+}: NavbarProps) => {
   const isDesktopMode = useMediaQuery("(min-width: 768px)");
   const [isMenuToggled, setIsMenuToggled] = useState(false);
+  const navbarBackground = isTopOfPage ? "" : "bg-red";
 
   return (
-    <nav className="z-40 w-full fixed top-0 py-6">
+    <nav className={`z-40 w-full fixed top-0 py-6 ${navbarBackground}`}>
       <div className="flex items-center justify-between mx-auto w-5/6">
         <div>
           <h4 className="font-playfair text-3xl font-bold">VS</h4>

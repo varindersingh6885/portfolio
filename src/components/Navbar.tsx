5d4295c8cf52d21df2a9f594d76f5ec67db8d8b8
Varinder Singh <varindersingh6885@email.com>
2023-09-03 21:06:21 +0530

    add navigation menu for small screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
 import useMediaQuery from "../hooks/useMediaQuery";
 import NavbarLink from "./NavbarLink";
+
+import MenuIcon from "../assets/menu-icon.svg";
+import CloseIcon from "../assets/close-icon.svg";
 
 type NavbarProps = {
   selectedPage: string;
@@ -25,8 +29,9 @@ const links = [
 ];
 
 const Navbar = ({ selectedPage, setSelectedPage }: NavbarProps) => {
-  const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
-  console.log(isAboveSmallScreens);
+  const isDesktopMode = useMediaQuery("(min-width: 768px)");
+  const [isMenuToggled, setIsMenuToggled] = useState(false);
+
   return (
     <nav className="z-40 w-full fixed top-0 py-6">
       <div className="flex items-center justify-between mx-auto w-5/6">
@@ -34,7 +39,7 @@ const Navbar = ({ selectedPage, setSelectedPage }: NavbarProps) => {
           <h4 className="font-playfair text-3xl font-bold">VS</h4>
         </div>
 
-        {isAboveSmallScreens ? (
+        {isDesktopMode ? (
           <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
             {links.map((link) => (
               <NavbarLink
@@ -46,7 +51,37 @@ const Navbar = ({ selectedPage, setSelectedPage }: NavbarProps) => {
             ))}
           </div>
         ) : (
-          <div>toggler</div>
+          <div>
+            <button
+              className="rounded-full bg-red p-2"
+              onClick={() => setIsMenuToggled(!isMenuToggled)}
+            >
+              <img src={MenuIcon} alt="menu-icon" />
+            </button>
+          </div>
+        )}
+
+        {!isDesktopMode && isMenuToggled && (
+          <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+            {/* CLOSE ICON */}
+            <div className="flex justify-end p-12">
+              <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                <img alt="close-icon" src={CloseIcon} title="close" />
+              </button>
+            </div>
+
+            {/* MENU ITEMS */}
+            <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
+              {links.map((link) => (
+                <NavbarLink
+                  key={link.name}
+                  page={link.name}
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+              ))}
+            </div>
+          </div>
         )}
       </div>
     </nav>
